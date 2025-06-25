b14e1d4aafcc984c583aa7ff9d4742dc0093c766
Vicente Lizana <lizanavicente48@gmail.com>
2025-06-25 17:17:24 -0400

    Delete Evaluación_Unidad_3 directory

--- Evaluación_Unidad_3/src/almacen/Observador.java
+++
@@ -1,9 +0,0 @@
-package almacen;
-
-// Observer: define el contrato para los observadores que desean recibir
-// notificaciones de cambios en el almacén.
-// Cualquier clase que implemente Observador debe definir el método actualizar,
-// que será llamado cuando el almacén notifique un evento.
-public interface Observador {
-    void actualizar(String mensaje);
-}
